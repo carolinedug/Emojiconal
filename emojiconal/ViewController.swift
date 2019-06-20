b29f3c17e59649b432be1ee2a419870e65ebe82a
Apple <apple@Apples-MacBook-Pro-5.local>
2019-06-20 15:30:42 -0500

    First commit

---
+++ emojiconal/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  emojiconal
+//
+//  Created by Apple on 6/20/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "alert", message: "hey",preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil) 
+    }
+}
+
